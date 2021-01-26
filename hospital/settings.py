8d0733d453493ec3936e74a54d47a69def9843b4
Mohamed <mohamedelshazly18@yahoo.com>
2021-01-26 13:10:30 +0200

    email verification added

--- hospital/settings.py
+++ hospital/settings.py
@@ -158,7 +158,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'omniscientist18@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASS']
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
 EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
